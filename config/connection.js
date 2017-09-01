52cfb54f2def50a04e9aad97e0fbd83ce0a07680
Stephen Hopkins <raidofive@gmail.com>
2017-09-01 07:39:58 -0400

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if(process.env.JAWSDB_URL) {
         root: 3000,
         host: 'localhost',
         user: 'root',
-        password: 'jsmlime13',
+        password: '',
         database: 'burgers_db',
     });
 };
